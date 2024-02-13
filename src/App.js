3b90dc1abda5080ad73ec79ef1e584607c4ca99b
unknown <tejamandhadapu197@gmail.com>
2024-02-13 10:49:32 +0530

    portfolio completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { Suspense, lazy, useEffect } from "react";
+import Hero from "./comp/Hero";
+import About from "./comp/About";
+import Skills from "./comp/Skills";
+import Project from "./comp/Project";
+import Hireme from "./comp/Hireme";
+import Contact from "./comp/Contact";
+import Foot from "./comp/Foot";
+import Navbar from "./comp/Navbar";
+import Bot from "./comp/Bot";
+
+
+
+
+
+const App=() =>{
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='bg-gray-900'>
+    <Navbar/>
+    {/* <Bot /> */}
+    <Hero />
+
+    <About/>
+    <Skills/>
+    {/* <Hireme/> */}
+    <Project/>
+    <Contact/>
+    <Foot/>
+  </div>
   );
-}
+};
 
 export default App;
+
+
